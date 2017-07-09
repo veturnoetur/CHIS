80ac89eaa41a94c5411cf9c4902532e5f53a6fc6
veturnoetur <candace.chow@giub.unibe.ch>
2017-07-09 20:51:24 +0200

    new

--- CHIS_ggplot2.R
+++ CHIS_ggplot2.R
@@ -7,46 +7,46 @@ library(ggthemes)
 # Script generalized into a function
 mosaicGG <- function(data, X, FILL) {
   
-  # Proportions in raw data
-  DF <- as.data.frame.matrix(table(data[[X]], data[[FILL]]))
-  DF$groupSum <- rowSums(DF)
-  DF$xmax <- cumsum(DF$groupSum)
-  DF$xmin <- DF$xmax - DF$groupSum
-  DF$X <- row.names(DF)
-  DF$groupSum <- NULL
-  DF_melted <- melt(DF, id = c("X", "xmin", "xmax"), variable.name = "FILL")
-  DF_melted <- DF_melted %>%
-    group_by(X) %>%
-    mutate(ymax = cumsum(value/sum(value)),
-           ymin = ymax - value/sum(value))
+# Proportions in raw data
+DF <- as.data.frame.matrix(table(data[[X]], data[[FILL]]))
+DF$groupSum <- rowSums(DF)
+DF$xmax <- cumsum(DF$groupSum)
+DF$xmin <- DF$xmax - DF$groupSum
+DF$X <- row.names(DF)
+DF$groupSum <- NULL
+DF_melted <- melt(DF, id = c("X", "xmin", "xmax"), variable.name = "FILL")
+DF_melted <- DF_melted %>%
+  group_by(X) %>%
+  mutate(ymax = cumsum(value/sum(value)),
+         ymin = ymax - value/sum(value))
   
-  # Chi-sq test
-  results <- chisq.test(table(data[[FILL]], data[[X]])) # fill and then x
-  resid <- melt(results$residuals)
-  names(resid) <- c("FILL", "X", "residual")
+# Chi-sq test
+results <- chisq.test(table(data[[FILL]], data[[X]])) # fill and then x
+resid <- melt(results$residuals)
+names(resid) <- c("FILL", "X", "residual")
   
-  # Merge data
-  DF_all <- merge(DF_melted, resid)
+# Merge data
+DF_all <- merge(DF_melted, resid)
   
-  # Positions for labels
-  DF_all$xtext <- DF_all$xmin + (DF_all$xmax - DF_all$xmin)/2
-  index <- DF_all$xmax == max(DF_all$xmax)
-  DF_all$ytext <- DF_all$ymin[index] + (DF_all$ymax[index] - DF_all$ymin[index])/2
+# Positions for labels
+DF_all$xtext <- DF_all$xmin + (DF_all$xmax - DF_all$xmin)/2
+index <- DF_all$xmax == max(DF_all$xmax)
+DF_all$ytext <- DF_all$ymin[index] + (DF_all$ymax[index] - DF_all$ymin[index])/2
   
-  # plot:
-  g <- ggplot(DF_all, aes(ymin = ymin,  ymax = ymax, xmin = xmin,
-                          xmax = xmax, fill = residual)) +
-    geom_rect(col = "white") +
-    geom_text(aes(x = xtext, label = X),
-              y = 1, size = 3, angle = 90, hjust = 1, show.legend = FALSE) +
-    geom_text(aes(x = max(xmax),  y = ytext, label = FILL),
-              size = 3, hjust = 1, show.legend = FALSE) +
+# plot:
+g <- ggplot(DF_all, aes(ymin = ymin,  ymax = ymax, xmin = xmin,
+                        xmax = xmax, fill = residual)) +
+geom_rect(col = "white") +
+geom_text(aes(x = xtext, label = X),
+          y = 1, size = 3, angle = 90, hjust = 1, show.legend = FALSE) +
+          geom_text(aes(x = max(xmax),  y = ytext, label = FILL),
+                    size = 3, hjust = 1, show.legend = FALSE) +
     scale_fill_gradient2("Residuals") +
     scale_x_continuous("Individuals", expand = c(0,0)) +
     scale_y_continuous("Proportion", expand = c(0,0)) +
     theme_tufte() +
     theme(legend.position = "bottom")
-  print(g)
+print(g)
 }
 
 # BMI described by age
